6ca37c1e7ec44347ac8e486e453ac3a4def2f7c1
João Paulo G. Brandt <joao@miro.com>
2022-04-21 13:18:55 +0200

    Add println to show stream creation and closing

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -2,6 +2,7 @@ import io.grpc.Server
 import io.grpc.ServerBuilder
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
 import test_bidi_stream.BidiStream
 import test_bidi_stream.RPCBidiStreamGrpcKt
 import test_bidi_stream.res
@@ -26,9 +27,14 @@ class Server {
 class BidiServer : RPCBidiStreamGrpcKt.RPCBidiStreamCoroutineImplBase() {
 
     override fun go(requests: Flow<BidiStream.Req>): Flow<BidiStream.Res> {
-        return requests.map { res {
-            id = it.id
-            r = it.s.substring(it.i until it.i + it.c).repeat(it.n)
-        } }
+        println("Created a stream.")
+        return requests
+            .map { res {
+                id = it.id
+                r = it.s.substring(it.i until it.i + it.c).repeat(it.n)
+            } }
+            .onCompletion {
+                println("Closing a stream.")
+            }
     }
 }
