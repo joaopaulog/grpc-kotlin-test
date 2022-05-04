0ae9e78c4f0520ce39956bc34b92db59c008160d
João Paulo G. Brandt <joao@miro.com>
2022-05-04 12:56:05 +0200

    Create ImprovedServerCalls
    Adjusted the code from io.grpc.kotlin.ServerCalls to add a buffer() in the response flow to bidirectional streams

--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,15 +1,19 @@
+import grpc.ImprovedServerCalls
 import grpc.ServerLogInterceptor
 import io.grpc.Server
 import io.grpc.ServerBuilder
+import io.grpc.ServerServiceDefinition
+import io.grpc.kotlin.AbstractCoroutineServerImpl
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import test_bidi_stream.BidiStream
-import test_bidi_stream.RPCBidiStreamGrpcKt
+import test_bidi_stream.RPCBidiStreamGrpc
 import test_bidi_stream.res
 import java.lang.Integer.max
 import java.lang.Integer.min
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 class Server {
     fun run(port: Int, verbose: Boolean) {
@@ -32,9 +36,9 @@ class Server {
     }
 }
 
-class BidiServer : RPCBidiStreamGrpcKt.RPCBidiStreamCoroutineImplBase() {
+class BidiServer(coroutineContext: CoroutineContext = EmptyCoroutineContext) : AbstractCoroutineServerImpl(coroutineContext) {
 
-    override fun go(requests: Flow<BidiStream.Req>): Flow<BidiStream.Res> {
+    fun go(requests: Flow<BidiStream.Req>): Flow<BidiStream.Res> {
         println("Created a stream.")
         return requests
             .map {
@@ -45,9 +49,18 @@ class BidiServer : RPCBidiStreamGrpcKt.RPCBidiStreamCoroutineImplBase() {
                         .repeat(max(1, it.n))
                 }
             }
-            .buffer(1024)
             .onCompletion {
                 println("Closing a stream.")
             }
     }
+
+    override fun bindService(): ServerServiceDefinition = ServerServiceDefinition
+        .builder(RPCBidiStreamGrpc.getServiceDescriptor())
+        .addMethod(
+            ImprovedServerCalls.bidiStreamingServerMethodDefinition(
+                context = this.context,
+                descriptor = RPCBidiStreamGrpc.getGoMethod(),
+                implementation = ::go
+            )
+        ).build()
 }
---
+++ src/main/kotlin/grpc/ImprovedServerCalls.kt
@@ -0,0 +1,203 @@
+package grpc
+
+import io.grpc.*
+import io.grpc.kotlin.GrpcContextElement
+import io.grpc.kotlin.ServerCalls.clientStreamingServerMethodDefinition
+import io.grpc.kotlin.ServerCalls.serverStreamingServerMethodDefinition
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.coroutines.CoroutineContext
+
+object ImprovedServerCalls {
+
+    /**
+     * Creates a [ServerMethodDefinition] that implements the specified bidirectional-streaming RPC
+     * method by running the specified implementation and associated implementation details within a
+     * per-RPC [CoroutineScope] generated with the specified [CoroutineContext].
+     *
+     * When the RPC is received, this method definition will pass a [Flow] of requests from the client
+     * to [implementation], and collect the returned [Flow], sending responses to the client as they
+     * are emitted.
+     *
+     * Exceptions and cancellation are handled as in [clientStreamingServerMethodDefinition] and as
+     * in [serverStreamingServerMethodDefinition].
+     *
+     * @param context The context of the scopes the RPC implementation will run in
+     * @param descriptor The descriptor of the method being implemented
+     * @param implementation The implementation of the RPC method
+     */
+    fun <RequestT, ResponseT> bidiStreamingServerMethodDefinition(
+        context: CoroutineContext,
+        descriptor: MethodDescriptor<RequestT, ResponseT>,
+        implementation: (requests: Flow<RequestT>) -> Flow<ResponseT>
+    ): ServerMethodDefinition<RequestT, ResponseT> {
+        require(descriptor.type == MethodDescriptor.MethodType.BIDI_STREAMING) {
+            "Expected a bidi streaming method descriptor but got $descriptor"
+        }
+        return serverMethodDefinition(context, descriptor, implementation)
+    }
+
+    /**
+     * Builds a [ServerMethodDefinition] that implements the specified RPC method by running the
+     * specified channel-based implementation within the specified [CoroutineScope] (and/or a
+     * subscope).
+     */
+    private fun <RequestT, ResponseT> serverMethodDefinition(
+        context: CoroutineContext,
+        descriptor: MethodDescriptor<RequestT, ResponseT>,
+        implementation: (Flow<RequestT>) -> Flow<ResponseT>
+    ): ServerMethodDefinition<RequestT, ResponseT> =
+        ServerMethodDefinition.create(
+            descriptor,
+            serverCallHandler(context, implementation)
+        )
+
+    /**
+     * Returns a [ServerCallHandler] that implements an RPC method by running the specified
+     * channel-based implementation within the specified [CoroutineScope] (and/or a subscope).
+     */
+    private fun <RequestT, ResponseT> serverCallHandler(
+        context: CoroutineContext,
+        implementation: (Flow<RequestT>) -> Flow<ResponseT>
+    ): ServerCallHandler<RequestT, ResponseT> =
+        ServerCallHandler {
+                call, _ ->
+            improvedServerCallListener(
+                context
+//                        + CoroutineContextServerInterceptor.COROUTINE_CONTEXT_KEY.get() // CHANGE: TODO: Not necessary right now
+                        + GrpcContextElement.current(),
+                call,
+                implementation
+            )
+        }
+
+    private fun <RequestT, ResponseT> improvedServerCallListener(
+        context: CoroutineContext,
+        call: ServerCall<RequestT, ResponseT>,
+        implementation: (Flow<RequestT>) -> Flow<ResponseT>
+    ): ServerCall.Listener<RequestT> {
+
+        val requestsChannel = Channel<RequestT>(Channel.UNLIMITED)
+        val requestFlow = createRequestsFlow(call, requestsChannel)
+
+        val readinessSignal = Readiness { call.isReady }
+        val requestProcessingJob = CoroutineScope(context).launch {
+
+            val mutex = Mutex()
+
+            val headersSent = AtomicBoolean(false)
+            suspend fun sendHeaders() {
+                if (headersSent.compareAndSet(false, true)) {
+                    mutex.withLock { call.sendHeaders(Metadata()) }
+                }
+            }
+
+            val failure = runCatching {
+                implementation(requestFlow)
+                    .buffer() // <------ IMPROVEMENT. Separate the coroutine of running the request processing, from the actual sending the response.
+                    .collect {
+                        sendHeaders()
+                        readinessSignal.suspendUntilReady()
+                        mutex.withLock { call.sendMessage(it) }
+                    }
+            }.exceptionOrNull()
+
+            sendHeaders()
+
+            val closeStatus = when (failure) {
+                null -> Status.OK
+                is CancellationException -> Status.CANCELLED.withCause(failure)
+                else -> Status.fromThrowable(failure)
+            }
+            val trailers = failure?.let { Status.trailersFromThrowable(it) } ?: Metadata()
+
+            mutex.withLock { call.close(closeStatus, trailers) }
+        }
+
+        return StreamServerCallListener(
+            call,
+            requestProcessingJob,
+            requestsChannel,
+            readinessSignal
+        )
+    }
+
+    private fun <RequestT, ResponseT> createRequestsFlow(call: ServerCall<RequestT, ResponseT>, requestsChannel: ReceiveChannel<RequestT>): Flow<RequestT> {
+        val requestsStarted = AtomicBoolean(false)
+        return flow {
+            check(requestsStarted.compareAndSet(false, true)) { "requests flow can only be collected once" }
+
+            call.request(1)
+            try {
+                for (request in requestsChannel) {
+                    emit(request)
+                    call.request(1)
+                }
+            } catch (e: Exception) {
+                requestsChannel.cancel(CancellationException("Exception thrown while collecting requests", e))
+                call.request(1)
+                throw e
+            }
+        }
+    }
+
+    private class StreamServerCallListener<RequestT, ResponseT>(
+        private val call: ServerCall<RequestT, ResponseT>,
+        private val requestProcessingJob: Job,
+        private val requestsChannel: SendChannel<RequestT>,
+        private val readinessSignal: Readiness,
+    ) : ServerCall.Listener<RequestT>() {
+
+        private var isReceiving : Boolean = true
+
+        override fun onCancel() {
+            requestProcessingJob.cancel("Cancellation received from client")
+        }
+
+        override fun onMessage(message: RequestT) {
+            if (isReceiving) {
+                requestsChannel.trySend(message)
+                    .onClosed { isReceiving = false }
+                    .onFailure {
+                        throw Status.INTERNAL
+                            .withDescription("onMessage should never be called when requestsChannel is unready")
+                            .asException()
+                    }
+            }
+            if (!isReceiving) {
+                call.request(1)
+            }
+        }
+
+        override fun onHalfClose() {
+            requestsChannel.close()
+        }
+
+        override fun onReady() {
+            readinessSignal.onReady()
+        }
+    }
+
+    private class Readiness(private val isReallyReady: () -> Boolean) {
+        private val channel = Channel<Unit>(Channel.CONFLATED)
+
+        fun onReady() {
+            channel.trySend(Unit)
+                .onFailure { throw AssertionError("Should be impossible; a CONFLATED channel should never return false on offer") }
+        }
+
+        suspend fun suspendUntilReady() {
+            while (!isReallyReady()) {
+                channel.receive()
+            }
+        }
+    }
+
+}
